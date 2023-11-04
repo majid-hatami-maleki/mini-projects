046b7a44940305fcebf73bd4334a8596c3eb9a6e
majid hatami maleki <HH.kral@outlook.com>
2023-11-04 15:52:20 +0330

    fixing apps header menu

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React, { useContext } from "react";
 import "./app.css";
-import { BrowserRouter,Link,NavLink } from "react-router-dom";
+import { BrowserRouter, Link, NavLink } from "react-router-dom";
 import Routers from "../routes/Routers";
 import Header from "./header/Header";
+import AppsHeader from "./appsHeader/AppsHeader";
+
 /*
 1- install react bootstrap for responsive
 2- introduce home screen
@@ -11,9 +13,10 @@ export default function App() {
   return (
     <div className="app-container">
       <BrowserRouter>
-      <Header/>
+        <Header />
+        <AppsHeader />
         <main>
-            <Routers/>
+          <Routers />
         </main>
       </BrowserRouter>
     </div>
---
+++ src/components/appsHeader/AppsHeader.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './header.css'
+export default function AppsHeader() {
+  return (
+    <div className='a-header-container'>
+        <ul>
+            <li>project 1</li>
+            <li>project 2</li>
+            <li>project 3</li>
+            <li>project 4</li>
+            <li>project 5</li>
+        </ul>
+    </div>
+  )
+}
