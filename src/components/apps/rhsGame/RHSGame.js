5e14f333d76a9c85547bcb1cbb8dc37c754f7a5d
majid hatami maleki <HH.kral@outlook.com>
2023-11-09 15:32:08 +0330

    rhs game finished just some styles must fix. starting extracker app

--- src/components/apps/rhsGame/RHSGame.js
+++ src/components/apps/rhsGame/RHSGame.js
@@ -1,12 +1,41 @@
-import React from 'react'
-import './rhs.css'
-import AppsHeader from '../../appsHeader/AppsHeader'
-import MainContainer from '../../appsContainer/MainContainer'
+import React, { useContext } from "react";
+import "./rhs.css";
+import AppsHeader from "../../appsHeader/AppsHeader";
+import MainContainer from "../../appsContainer/MainContainer";
+import { FaHandPaper, FaHandRock, FaHandScissors } from "react-icons/fa";
+import { RHSGameContexts } from "../../../contexts/RHSGameContext";
 
 export default function RHSGame() {
+  const contexts = useContext(RHSGameContexts);
+  console.log(contexts.actions);
   return (
     <MainContainer>
-        <AppsHeader class={'rhs-header'}/>
+      <AppsHeader class={"rhs-header"} />
+      <div className="rhs-container">
+        <div className="actions-result-display">
+          <div className="player-actions-result">
+            {contexts.playerActionResult}
+            <h2>player</h2>
+          </div>
+          <h1>{contexts.result}</h1>
+          <div className="cpu-actions-result">
+            {contexts.cpuActionResult}
+            <h2>Cpu</h2>
+            </div>
+        </div>
+        <div className="player-actions-options">
+          {contexts.actions.map((action) => (
+            <button
+              key={action.id}
+              onClick={() => {
+                contexts.actionsHandler(action);
+              }}
+            >
+              {action.icon}
+            </button>
+          ))}
+        </div>
+      </div>
     </MainContainer>
-  )
+  );
 }
--- src/contexts/RHSGameContext.js
+++ src/contexts/RHSGameContext.js
@@ -1,11 +1,73 @@
-import React, { createContext } from 'react'
-const RHSGameContexts = createContext() 
+import React, { createContext, useState } from "react";
+import { FaHandPaper, FaHandRock, FaHandScissors } from "react-icons/fa";
+const RHSGameContexts = createContext();
 
-export default function RHSGameContextProvider({children}) {
-  return (
-    <RHSGameContexts.Provider value={'this is RHS game'}>
-        {children}
-    </RHSGameContexts.Provider>
-  )
+const actions = [
+  {
+    id: 1,
+    name: "rock",
+    icon: <FaHandRock />,
+    fight : function (playerAction) {
+        if(this.name === playerAction.name){
+            return 'draw'
+        }else if(playerAction.name === 'scissor'){
+            return `CPU won`
+        }else{
+            return 'player won'
+        }
+    }   
+  },
+  {
+    id: 2,
+    name: "paper",
+    icon: <FaHandPaper />,
+    fight : function (playerAction) {
+        if(this.name === playerAction.name){
+            return 'draw'
+        }else if(playerAction.name === 'rock'){
+            return `CPU won`
+        }else{
+            return 'player won'
+        }
+    }  
+  },
+  {
+    name: "scissor",
+    id : 3,
+    icon: <FaHandScissors />,
+    fight : function (playerAction) {
+        if(this.name === playerAction.name){
+            return 'draw'
+        }else if(playerAction.name === 'paper'){
+            return `CPU won`
+        }else{
+            return 'player won'
+        }
+    }  
+  },
+];
+export default function RHSGameContextProvider({ children }) {
+const [playerActionResult, setPlayerActionResult] = useState()
+const [cpuActionResult , setCpuActionResult] = useState()
+const [result , setResult] = useState()
+
+
+function actionsHandler(action) {
+    let randomNumber = Math.floor(Math.random() * 3)
+    let cpuAction = actions[randomNumber]
+    setPlayerActionResult(action.icon)
+    setCpuActionResult(cpuAction.icon)
+    setResult(cpuAction.fight(action))
 }
-export {RHSGameContexts}
+
+  return (
+    <RHSGameContexts.Provider
+      value={{
+        actions,playerActionResult,cpuActionResult,actionsHandler,result
+      }}
+    >
+      {children}
+    </RHSGameContexts.Provider>
+  );
+}
+export { RHSGameContexts };
