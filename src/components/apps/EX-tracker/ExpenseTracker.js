06c1949aa760f6792d8332df84460b606764cf06
majid hatami maleki <HH.kral@outlook.com>
2023-11-10 11:50:52 +0330

    populating project jsx

--- src/components/apps/EX-tracker/ExpenseTracker.js
+++ src/components/apps/EX-tracker/ExpenseTracker.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './ExTracker.css'
 import AppsHeader from '../../appsHeader/AppsHeader'
 import MainContainer from '../../appsContainer/MainContainer'
-
+import { EXTrackerContexts } from '../../../contexts/ExpenseTrackerContext'
 export default function ExpenseTracker() {
+    const contexts = useContext(EXTrackerContexts)
+    console.log(contexts.addNewEvent())
   return (
     <MainContainer>
         <AppsHeader class={'Ex-header'}/>
--- src/contexts/ExpenseTrackerContext.js
+++ src/contexts/ExpenseTrackerContext.js
@@ -1,10 +1,30 @@
-import React, { createContext } from "react";
-
+import React, { createContext, useState } from "react";
 let EXTrackerContexts = createContext();
-
 function ExTrackerContextProvider({ children }) {
+  const dt = new Date();
+  const currTime = `${dt.getHours()}:${dt.getMinutes()}`;
+  const currDate = `${dt.getFullYear()}/${dt.getMonth()}/${dt.getDay()}`;
+  const [events, setEvents] = useState([]);
+  const [newEventPaySubject , setNewEventPaySubject] = useState('buy laptop')
+  const [newEventPaySource ,setNewEventPaySource] = useState('wallet')
+  const [newEventPayAmount , setNewEventPayAmount] = useState(12000)
+  function addNewEvent(params) {
+    let allEvents = [...events];
+    let newEvent = {};
+    newEvent.id = `event-` + allEvents.length + 1;
+    newEvent.time = currTime;
+    newEvent.date = currDate;
+    newEvent.paymentSubject = newEventPaySubject
+    newEvent.paymentSource = newEventPaySource
+    newEvent.paymentAmount = newEventPayAmount
+    console.log(newEvent);
+  }
   return (
-    <EXTrackerContexts.Provider value={"this is EXTrackerContexts"}>
+    <EXTrackerContexts.Provider
+      value={{
+        addNewEvent,
+      }}
+    >
       {children}
     </EXTrackerContexts.Provider>
   );
