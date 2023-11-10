e10d742d9f5f7a3aaf0546f590ff432778429696
majid hatami maleki <HH.kral@outlook.com>
2023-11-10 11:59:06 +0330

    jsx important parents added - adding childs

--- src/components/apps/EX-tracker/ExpenseTracker.js
+++ src/components/apps/EX-tracker/ExpenseTracker.js
@@ -1,15 +1,21 @@
-import React, { useContext } from 'react'
-import './ExTracker.css'
-import AppsHeader from '../../appsHeader/AppsHeader'
-import MainContainer from '../../appsContainer/MainContainer'
-import { EXTrackerContexts } from '../../../contexts/ExpenseTrackerContext'
+import React, { useContext } from "react";
+import "./ExTracker.css";
+import AppsHeader from "../../appsHeader/AppsHeader";
+import MainContainer from "../../appsContainer/MainContainer";
+import { EXTrackerContexts } from "../../../contexts/ExpenseTrackerContext";
 export default function ExpenseTracker() {
-    const contexts = useContext(EXTrackerContexts)
-    console.log(contexts.addNewEvent())
+  const contexts = useContext(EXTrackerContexts);
+  console.log(contexts.addNewEvent());
   return (
     <MainContainer>
-        <AppsHeader class={'Ex-header'}/>
+      <AppsHeader class={"Ex-header"} />
+      <div className="ExTracker-container">
+        <div className="transaction-container">
+          <ul className="transaction-table"></ul>
+          <div className="newTransaction-panel"></div>
+        </div>
+        <ul className="logs-table"></ul>
+      </div>
     </MainContainer>
-  )
+  );
 }
-
